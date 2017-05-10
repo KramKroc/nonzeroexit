d41c85bf437b256d3df5c881c13c74a609b166d3
Mark Corkery <mark.corkery@gmail.com>
2017-05-10 22:16:16 +0100

    Updated for Spring Boot 1.5.3, Cloud Camden.SR6 and docker 1.13

--- sample-service/src/test/java/com/kramkroc/SampleServiceApplicationTests.java
+++ sample-service/src/test/java/com/kramkroc/SampleServiceApplicationTests.java
@@ -3,13 +3,13 @@ package com.kramkroc;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @Ignore
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = SampleServiceApplication.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SampleServiceApplication.class)
 @WebAppConfiguration
 public class SampleServiceApplicationTests {
 
